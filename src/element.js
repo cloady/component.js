d4f9330336d87c5f381c5ac7fc6b5c876610e75c
cloady <loggr.ru@yandex.ru>
2015-02-24 22:05:37 +0300

    Update element.js

--- src/element.js
+++ src/element.js
@@ -1,72 +1,48 @@
 (function() {
-    var Element = this.Class.extend(function(el) {
-        if (typeof(el) === 'string') el = document.querySelector(el);
-        this.el = el;
-    };
-    
-    Element.prototype.getStyle = function(k) {
-        return (this.el.style[k]) ? this.el.style[k] : window.getComputedStyle(this.el)[k]; 
-    };
 
-    Element.prototype.setStyle = function(k,v) {
-        this.el.style[k] = v;
-    };
-    
-    Element.prototype.addClass = function(cls) {
-        if (this.el.classList)
-            this.el.classList.add(cls);
-        else if(!this.hasClass(cls))
-            this.el.className += ' '+cls;
-    };
-    
-    Element.prototype.hasClass = function(cls) {
-        if (this.el.classList)
-            return this.el.classList.contains(cls);
-        else
-            return (this.el.className.split(' ').indexOf(cls) === -1) ? false : true;
-    };
-    
-    Element.prototype.show = function(as) {
-        if (!as) as = 'block';
-        this.el.style.display = as;
-    };
-    
-    Element.prototype.hide = function() {
-        this.el.style.display = 'none';
-    };
-    
-    Element.prototype.isVisible = function() {
-        return (this.getStyle('display') !== 'none');
-    };
-    
-    Element.prototype.removeClass = function(cls) {
-        if (this.el.classList)
-            return this.el.classList.remove(cls);
-        else
-        {
-            var classList = this.el.className.split(' ');
-            if (classList.indexOf(cls) !== -1) 
-                classList.splice(classList.indexOf(cls), 1);
-            this.el.className = classList.join(' ');
-        }
-    };
-    
-    Element.prototype.next = function() {
-        return this.el.nextElementSibling;
-    };
-    
-    Element.prototype.prev = function() {
-        return this.el.previousElementSibling;
-    };
-    
-    Element.prototype.children = function(cls) {
-        return this.el.children;
-    };
-    
-    Element.prototype.remove = function() {
-        this.el.parentNode.removeChild(this.el);
-    };
-    
-    this.Element = Element;
-    
+	this.Container = (function() {
+		  function Container(config) {
+		  	this._children = (config.children) ? config.children : [];
+		  	this._el = (config.el) ? config.el : null;
+		  }
+
+		  Container.prototype.indexOf = function(e) {
+		  	for(var key in this._children)
+		  		if (this._children[key])
+		  };
+
+		  Container.prototype.getAt = function(index) {
+		  	return this._children[index];
+		  };
+
+		  Container.prototype.render = function(data) {
+		  		for(var key in this._children) 
+		  			this._children[key] = this.renderItem(data);
+		  };
+
+		  Container.prototype.select = function(e) {
+		  	this.trigger('select', e);
+		  };
+
+		  Container.prototype.deselect = function(e) {
+		  	this.trigger('deselect', e);
+		  };
+
+		  Container.prototype.on = function(evt, method) {
+		  	this._el.addEventListener(evt, method, true);
+		  };
+
+		  Container.prototype.off = function(evt, method) {
+		  	this._el.removeEventListener(evt, method);
+		  };
+
+		  Container.prototype.trigger = function() {
+		  	(document.createEvent) 
+		  		? this._el.dispatchEvent(event)
+		  		: this._el.fireEvent("on" + event.eventType, event);
+		  };
+
+		  return Container;
+	})();
+
 }).call(window);
