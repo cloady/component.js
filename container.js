c4c6816366ce520db869270edb50c1e20b705b63
cloady <loggr.ru@yandex.ru>
2016-04-25 19:45:30 +0300

    component.js
    Reorg

--- container.js
+++ container.js
@@ -1,47 +1,21 @@
-(function() {
-	this.Container = (function() {
-		  function Container(config) {
-		  	((!config) && (config = {}));
-		  	this._children = (config.children) ? config.children : [];
-		  	this._el = (config.el) ? config.el : null;
-		  }
+var component = (function() {
+    var components = [];
 
-		  Container.prototype.indexOf = function(e) {
-		  	for(var key in this._children)
-		  		if (this._children[key] === e) return key;
-		  };
+    return function(id, component) {
+        if (typeof(id) != 'string') {
+            component = id;
+            id = null;
+        }
 
-		  Container.prototype.getAt = function(index) {
-		  	return this._children[index];
-		  };
+        if (typeof(component) == 'function')
+            component = new component;
 
-		  Container.prototype.render = function(data) {
-		  		for(var key in this._children) 
-		  			this._children[key] = this.renderItem(data);
-		  };
-
-		  Container.prototype.select = function(e) {
-		  	this.trigger('select', e);
-		  };
-
-		  Container.prototype.deselect = function(e) {
-		  	this.trigger('deselect', e);
-		  };
-
-		  Container.prototype.on = function(evt, method) {
-		  	this._el.addEventListener(evt, method, true);
-		  };
-
-		  Container.prototype.off = function(evt, method) {
-		  	this._el.removeEventListener(evt, method);
-		  };
-
-		  Container.prototype.trigger = function() {
-		  	(document.createEvent) 
-		  		? this._el.dispatchEvent(event)
-		  		: this._el.fireEvent("on" + event.eventType, event);
-		  };
-
-		  return Container;
-	})();
-}).call(window);
+        if (id) {
+            if (component)
+                components[id] = component;
+            return components[id];
+        } else {
+            return component;
+        }
+    };
+})();
